1bdabb8dd9f5394d148a7a38835e071904613d81
lucascharrer <62573614+lucascharrer@users.noreply.github.com>
2020-08-30 17:21:01 -0700

    Added all of the files

---
+++ harmonicity.m
@@ -0,0 +1,55 @@
+function [approxFrequency,SSR] = harmonicity(x,pot,cutoffV,prefix)
+%If you input a 1D potential which is roughly harmonic and roughly
+%centered, this function takes that potential, finds the center and sets it to zero,
+%and cuts off anything higher than the cutoffV above the center. So, that gives you
+%a symmetric potential where the center is 0. It then fits a harmonic potential,
+%prints out the frequency of that harmonic, and returns the sum of the
+%squared residuals as a measure of how close the original potential is to
+%harmonic, and plots the original pot and the approximation
+if nargin < 3
+    cutoffV = 0.1;
+end
+if nargin < 4
+    prefix = "";
+end
+
+[minimumV,minIndex] = min(pot);
+renormPot = pot - minimumV;
+
+potMinusCutoff = abs(renormPot - cutoffV);
+
+[~,cutoffIndex] = min(potMinusCutoff);
+
+delta = abs(minIndex - cutoffIndex) - 5;
+
+iStart = minIndex - delta;
+iEnd = minIndex + delta;
+
+xSym = x(iStart:iEnd);
+potSym = renormPot(iStart:iEnd);
+
+p = polyfit(xSym,potSym,2);
+
+k = 2 * p(1) * 1e6 * 1.602e-19;
+m = 1.46127438e-25; % Mass of strontium
+omega = sqrt( k / m );
+
+approxFrequency = omega / (2*pi);
+
+approxHarmonic = polyval(p,xSym);
+residuals = approxHarmonic - potSym;
+resSquared = residuals .^2;
+SSR = sum(resSquared);
+figure;
+plot(xSym,potSym);
+hold on
+plot(xSym,approxHarmonic);
+legend('Actual Potential','Best-fit Harmonic');
+freqString = sprintf('Frequency: %0.3f kHz',approxFrequency*1e-3);
+SSRString = sprintf('Sum of Squared Residuals: %0.5e',SSR);
+titleString = prefix + freqString + "\n" + SSRString;
+titleString = compose(titleString);
+title(titleString);
+
+
+
